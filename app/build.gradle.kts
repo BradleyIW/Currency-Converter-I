41b772ca69e2cf3a6dd87f7ab3c2d489505ebc23
BradleyWire <bradley.wilson@wire.com>
2020-06-14 13:43:13 +0200

    Migrated default Gradle Groovy scripts to use Kotlin DSL

---
+++ app/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id(BuildPlugins.androidApplication)
+    id(BuildPlugins.kotlinAndroid)
+    id(BuildPlugins.kotlinAndroidExtensions)
+}
+
+android {
+    compileSdkVersion(AndroidSdk.compile)
+
+    defaultConfig {
+        applicationId = appId
+        minSdkVersion(AndroidSdk.min)
+        targetSdkVersion(AndroidSdk.target)
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = TestLibraries.testRunner
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+}
+
+dependencies {
+    implementation(Libraries.kotlinStdLib)
+    implementation(Libraries.appCompat)
+    implementation(Libraries.ktxCore)
+    implementation(Libraries.constraint)
+
+    testImplementation(TestLibraries.junit4)
+}
